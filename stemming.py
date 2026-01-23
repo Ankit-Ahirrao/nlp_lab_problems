7ef5110df481e0cc4282681bb19d6ad42cd03888
Ankit Ahirrao <ahirraoankit5@gmail.com>
2026-01-23 11:33:13 +0530

    NLP vectorization one hot bow tfidf implement

--- stemming.py
+++ stemming.py
@@ -1,9 +1,13 @@
-from nltk.stem import PorterStemmer
+# from nltk.stem import PorterStemmer
 
-stemmer = PorterStemmer()
+# stemmer = PorterStemmer()
 
-words = ["playing", "played", "playfully", "studies", "study", "studying", "better"]
+# words = ["hindered", "played", "playfully", "studies", "study", "studying", "better"]
 
-for word in words:
-    stemmer_word = stemmer.stem(word)
-    print(word, "->", stemmer_word)
+# for word in words:
+#     stemmer_word = stemmer.stem(word)
+#     print(word, "->", stemmer_word)
+
+import re
+pattern = r"\w+(?:’\w+)?|\d+|[^\w\s]"
+print(re.findall(pattern, "I’m learning NLP in 2026!"))
